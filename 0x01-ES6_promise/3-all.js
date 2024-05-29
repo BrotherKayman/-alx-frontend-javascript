701323d0d59c4733b7d747859708c8140520dfb1
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-05-29 22:23:08 +0200

    remove async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const value = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-  } catch {
-    return console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+
+    .catch(() => console.log('Signup system offline'));
 }
