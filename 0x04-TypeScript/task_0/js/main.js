b39c0f05abefa4fa961f64921cddf512c6ba70ca
Kagiso (BrotherKayman) Motlhaoleng <135019661+BrotherKayman@users.noreply.github.com>
2024-06-05 23:23:54 +0200

    Update

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -1,1 +1,32 @@
-
+var student1 = {
+    firstName: "Kagiso",
+    lastName: "Kayman",
+    age: 30,
+    location: "Johannesburg"
+};
+var student2 = {
+    firstName: "Thapelo",
+    lastName: "Wick",
+    age: 26,
+    location: "Cape Town"
+};
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
+table.style.background = "blue";
+table.appendChild(tbody);
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    nameCell.style.border = "1px solid pink";
+    locationCell.style.border = "1px solid pink";
+    nameCell.style.padding = "5px";
+    locationCell.style.padding = "5px";
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+document.body.appendChild(table);
